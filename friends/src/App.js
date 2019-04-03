c883e646a0e958e7db9d910b3ca80863f6b5d375
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-04-02 20:25:49 -0700

    Completed MVP

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Friends from './Friends'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Friends />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ friends/src/Friends.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+
+class Friends extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            friends: []
+        };
+    }
+    componentDidMount(){
+        axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+            this.setState({friends: response.data})
+            console.log(this.state.friends)
+        })
+        .catch(err => console.log(err))
+    }
+
+    render() {
+        return(
+            <div>
+                <form>
+
+                </form>
+                <h2> Current Friends</h2>
+                <div>
+                   <p> {this.state.friends.map(friend => {
+                       return (
+                           <div> 
+                            <h3> {friend.name}</h3>
+                            <p> Is {friend.age} years old. </p>
+                            <p> E-mail: {friend.email}</p>
+
+                           </div>
+                       )
+                   })}</p> 
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Friends;
