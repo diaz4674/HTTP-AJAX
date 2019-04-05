40449ffd0c55fe60ad42c17eb725f659e1bbab71
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-04-04 20:57:57 -0700

    Completed MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Friends from './Friends';
 import axios from 'axios';
 import {Route} from 'react-router-dom';
+import Form from './components/Form'
 
 class App extends Component {
   constructor(){
@@ -23,11 +24,24 @@ updateFriend = newFriend => {
   this.setState({friends: newFriend});
 };
 
+deleteFriend = id => {
+
+  axios
+    .delete(`http://localhost:5000/friends/${id}`)
+    .then(res => {
+      this.setState({friends: res.data})
+    })
+    .catch(err => {console.log(err)})
+}
+
 
   render() {
     return (
       <div className="App">
-        <Route exact path = '/' render = {props => <Friends {...props} friends = {this.state.friends} updateFriend = {this.updateFriend}/>} />
+
+        <Route path = '/' render = {props => <Form {...props} updateFriend = {this.updateFriend}/>} />
+        <h2> Current Friends</h2>
+        <Route exact path = '/' render = {props => <Friends {...props} friends = {this.state.friends} updateFriend = {this.updateFriend} deleteFriend = {this.deleteFriend}/>} />
       </div>
     );
   }
--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,68 +1,17 @@
 import React from 'react';
-import axios from 'axios';
+import FriendsList from './components/FriendsList';
 
-class Friends extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            friend: {
-            name: '',
-            age: '',
-            email: '',
-            id: ''
-            }
-        }
-    }
 
-    changeHandler = e => {
-        e.persist();
-        let value = e.target.value;
-        this.setState(prevState => ({friend: {...prevState.friend, [e.target.name]: value}
-        }));
-    }
+const Friends = props => {
 
-    handleSubmit = e => {
-        e.preventDefault();
-        axios
-            .post('http://localhost:5000/friends', this.state.friend )
-            .then(response => {
-                this.props.updateFriend(response.data);
-                this.props.history.push('/')
-                e.persist();
-            })
-            .catch(err => console.log(err))
-    }
 
-     deleteFriend = e => {
-         console.log(`http://localhost:5000/friends/${e.id}`)
 
-    }
-
-    render(){
     return (
-        <div>
-        <form onSubmit = {this.handleSubmit}>
-            <input type ='text' name = 'name' onChange = {this.changeHandler} placeholder = 'Name' value = {this.state.friend.name}/> 
-            <input type ='text' name = 'age' onChange = {this.changeHandler} placeholder = 'Age' value = {this.state.friend.age}/> 
-            <input type ='text' name = 'email' onChange = {this.changeHandler} placeholder = 'Email' value = {this.state.friend.email}/> 
-        <button type = 'submit' > Add Friend </button>
-        </form>
-        <h2> Current Friends</h2>
-        <div>
-            {this.props.friends.map((friend, id) => {
-               return (
-                   <div > 
-                   <span onClick = {this.deleteFriend}  key = {friend.id}> X </span>
-                    <h3 > {friend.name}</h3>
-                    <p> Is {friend.age} years old. </p>
-                    <p> E-mail: {friend.email}</p>
-                   </div>
-               )
-           })}
+        <div> 
+            {props.friends.map((friend, i) => <FriendsList friends = {friend} deleteFriend = {props.deleteFriend} key = {i}/>)}
+
         </div>
-    </div>
     )
     }
-}
 
 export default Friends;
